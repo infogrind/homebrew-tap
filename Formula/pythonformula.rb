8dc479083d219a3509442fa4aaa994d8b50f1287
Marius Kleiner <kleiner@gmail.com>
2025-03-30 17:55:48 +0200

    Update pythonformula to 1.01

--- Formula/pythonformula.rb
+++ Formula/pythonformula.rb
@@ -3,8 +3,8 @@ class Pythonformula < Formula
 
   desc "Convert a Python uv.lock file to a Homebrew formula dependency format"
   homepage "https://github.com/infogrind/pythonformula"
-  url "https://github.com/infogrind/pythonformula/archive/refs/tags/v1.0.tar.gz"
-  sha256 "f5ebe5aad09b8c8b57ddcdd025203afd0d1cd1efa535def3ac7d5744bb1a5f8d"
+  url "https://github.com/infogrind/pythonformula/archive/refs/tags/v1.01.tar.gz"
+  sha256 "be97968a50b9e0bb1ebb38164c8c69d5bfcd6b0680d69f659633f7c3c1283916"
   license "MIT"
 
   depends_on "python@3.13"
@@ -14,7 +14,7 @@ class Pythonformula < Formula
   end
 
   test do
-    assert_file_exists bin/"pythonformula"
+    assert_path_exists bin/"pythonformula"
     assert_match "",
       shell_output("echo 'version = 1' | #{bin}/pythonformula 2>&1")
   end
